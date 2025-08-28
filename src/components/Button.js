f09eb87737c37c9c79cf98ee1a35c9c8edae6e51
vijaykumar8020 <vijay.kumar@altudo.co>
2025-08-28 12:15:46 +0530

    Initial commit Remote Button Component Added

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <button style={{ padding: "10px", background: "blue", color: "white" }}>Remote Button</button>;
+};
+
+export default Button;
