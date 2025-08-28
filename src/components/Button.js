7cae05001b1d7cccd62c133ca3997f8bfc520dec
vijaykumar8020 <vijay.kumar@altudo.co>
2025-08-28 14:34:44 +0530

    console added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Button = () => {
+  console.log("Remote Button from CRA");
   return <button style={{ padding: "10px", background: "blue", color: "white" }}>Remote Button</button>;
 };
 
